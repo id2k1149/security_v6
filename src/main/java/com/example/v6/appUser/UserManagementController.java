0ad2f1fa11934da04c29a76491e9e3a81c501323
id2k1149 <id2k1149@gmail.com>
2021-07-28 12:20:37 -0400

    v03 @PreAuthorize

--- src/main/java/com/example/v6/appUser/UserManagementController.java
+++ src/main/java/com/example/v6/appUser/UserManagementController.java
@@ -1,5 +1,6 @@
 package com.example.v6.appUser;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,23 +17,27 @@ public class UserManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<AppUser> getAllUsers() {
         return APP_USERS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('user:write')")
     public void registerNewUser(@RequestBody AppUser user) {
         System.out.println("registerNewUser");
         System.out.println(user);
     }
 
     @DeleteMapping(path = "/{userId}")
+    @PreAuthorize("hasAuthority('user:write')")
     public void deleteUser(@PathVariable("userId") Integer userId) {
         System.out.println("deleteUser");
         System.out.println(userId);
     }
 
     @PutMapping(path = "/{userId}")
+    @PreAuthorize("hasAuthority('user:write')")
     public void updateUser(@PathVariable("userId") Integer userId,
                            @RequestBody AppUser user) {
         System.out.println("updateUser");
